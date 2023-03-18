48fb956e4e643eee34993a5b1236af32184b0933
Gabriel Rueda <ruedag@uwindsor.ca>
2023-03-18 19:02:13 -0400

    Finds max for each stock

--- main.py
+++ main.py
@@ -18,6 +18,32 @@ for ticker in tickerList:
         prices.append(price)
     tickerData[ticker] = prices
 
-print(tickerData)
+# print(tickerData)
+
+results = []
+
+for key in tickerData:
+    values = tickerData[key]
+    l = 0   
+    maxProfit = 0
+    buyDay = 0
+    sellDay = 0
+    # Increment right ptr every time
+    # move left ptr when l > r
+    for r in range(1, len(values)):
+        if(values[l] > values[r]):
+            l = r
+        else:
+            # Check if max profit is there
+            diff = values[r] - values[l]
+            if(diff > maxProfit):
+                maxProfit = diff
+                buyDay = l
+                sellDay = r
+
+    results.append([key, maxProfit, buyDay, sellDay])
+
+
+print(results)
 
 
